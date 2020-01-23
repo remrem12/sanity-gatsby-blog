e05063145fdc67546c477ae0f386fff3f9fd65cb
Nghĩa Hữu <buihuunghia.10hlvc@gmail.com>
2020-01-23 10:30:03 +0700

    update

--- web/src/html.js
+++ web/src/html.js
@@ -25,8 +25,6 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-
-
       </body>
     </html>
   )
