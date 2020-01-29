c4372907f8d0a7a0480af793afd3e1721ed1b27e
Nghĩa Hữu <buihuunghia.10hlvc@gmail.com>
2020-01-29 17:28:24 +0700

    edit about

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,67 +1,9 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import {
-  mapEdgesToNodes,
-  filterOutDocsWithoutSlugs,
-  filterOutDocsPublishedInTheFuture
-} from '../lib/helpers'
 import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
 
-export const query = graphql`
-query lastpostQuery {
-  post: allSanityPost(
-    limit: 1
-    sort: { fields: [publishedAt], order: DESC }
-  ) {
-    edges {
-      node {
-        _id
-        publishedAt
-        mainImage {
-          alt
-        }
-        _rawExcerpt
-        title
-        slug {
-          current
-        }
-      }
-    }
-  }
-  site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
-    title
-    description
-    keywords
-  }
-}
-`
-
 const AboutPage = props => {
-  const { data, errors } = props
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    )
-  }
-
-  const site = (data || {}).site
-  const postNodes = (data || {}).posts
-    ? mapEdgesToNodes(data.posts)
-      .filter(filterOutDocsWithoutSlugs)
-      .filter(filterOutDocsPublishedInTheFuture)
-    : []
-
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    )
-  }
-
   return (
     <Layout>
       <SEO
