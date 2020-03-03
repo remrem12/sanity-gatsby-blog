bb05c9067c363a506d77e5abf6c96bb24276f9fa
Nghĩa Hữu <buihuunghia.10hlvc@gmail.com>
2020-03-03 12:14:33 +0700

    Header -> header

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -12,7 +12,7 @@ import {Link} from 'gatsby'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import "./Header.css";
+import "./header.css";
 class Header extends Component {
   state = {
     isOpen: false
